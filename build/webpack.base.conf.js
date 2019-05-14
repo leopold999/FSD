c99a439adb37250fceef4eee37983d8c65dcd277
Leonid Kurachenko <aes-khv@yandex.ru>
2019-05-14 11:14:56 +0400

    Configured webpack

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,89 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin')
+
+
+const PATHS = {
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/'
+}
+
+module.exports = {
+  // BASE config
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    app: PATHS.src
+  },
+  output: {
+    filename: `${PATHS.assets}js/[name].js`,
+    path: PATHS.dist,
+    publicPath: '/'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/'
+    }, {
+      test: /\.(png|jpg|gif|svg)$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]'
+      }
+    }, {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }, {
+          loader: 'postcss-loader',
+          options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
+        }, {
+          loader: 'sass-loader',
+          options: { sourceMap: true }
+        }
+      ]
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }, {
+          loader: 'postcss-loader',
+          options: { sourceMap: true, config: { path: `${PATHS.src}/js/postcss.config.js` } }
+        }
+      ]
+    }, {
+      test: /\.pug$/,
+      loaders: 'pug-loader',
+      options: { pretty: true }
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.assets}css/[name].css`,
+    }),
+    // Copy HtmlWebpackPlugin and change index.html for another html page
+    new HtmlWebpackPlugin({
+      hash: false,
+      template: `${PATHS.src}/index.pug`,
+      filename: './index.html'
+    }),
+    new HtmlWebpackPugPlugin(),
+    new CopyWebpackPlugin([
+      { from: `${PATHS.src}/img`, to: `${PATHS.assets}img` },
+      { from: `${PATHS.src}/static`, to: '' },
+    ])
+  ],
+}
